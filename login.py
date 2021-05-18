3e649138c785d7f71cb9d456db19c1afafec6ec4
khadirzayan <84263835+khadirzayan@users.noreply.github.com>
2021-05-18 19:20:44 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,1 +1,10 @@
+accounts ={"test" : "test1", "test2" : "password"}
 
+username=input("please enter your username: ")
+password=input("please enter the password for "+ username + ": ")
+
+if(accounts.get(username) == password):
+    print("login succesful")
+    test=input(".")
+else:
+    print("wrong password")
